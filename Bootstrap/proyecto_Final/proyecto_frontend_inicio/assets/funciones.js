eff6f984f37253c7c4db3b69ee9fb320b742cc55
VivianaRamirez <ramirezvivi0102@gmail.com>
2021-02-02 21:15:03 -0500

    restaura consulta de ajax.

--- Bootstrap/proyecto_Final/proyecto_frontend_inicio/assets/funciones.js
+++ Bootstrap/proyecto_Final/proyecto_frontend_inicio/assets/funciones.js
@@ -1,28 +1,61 @@
 
-//  boton contacto 
+//  boton contacto
 function abrir(){
     $('#ventana').modal('show');
   }
-  
- 
-console.log('correcto');
-  
 
 function traerDatos(){
+    //Carga los datos que estan en el JSON (info.json) usando AJAX
+    $.ajax({
+		url: "datos.json",
+		method: "GET"
+	  }).done(function (resultado) {
 
-  var peticion = new XMLHttpRequest();
-  peticion.open('GET','http://127.0.0.1:5501/proyecto_Final/proyecto_frontend_inicio/datos.json');
-  peticion.onload = function(){
-      if (peticion.status == 200){
+		//Guarda el resultado en variables
+		hoy = resultado.fechaActual;
+		canciones= resultado.canciones;
 
-        console.long(peticion.response);
+		//Selecciona los eventos que sean anteriores a la fecha actual del JSON
+		for(var i = 0; i < canciones.length; i++){
+		  if (canciones[i].fecha < hoy){
+			canciones.push(canciones[i]);
+		  }
+		}
 
-      };
-  };
-  peticion.send();
+		//Ordena los eventos segun la fecha (los mas recientes primero)
+		canciones = canciones.sort(function(x,y){
+		  if (x.fecha < y.fecha){
+			return 1;
+		  }
+		  return -1;
+		});
+
+		//Crea un string que contenga el HTML que describe el detalle del evento
+		var html = "cancines.html"
+
+		//Recorre el arreglo y concatena el HTML para cada evento
+	  //   for(var j = 0; j < canciones.length; j++){
+	  //     html += `
+	  //             <h2>${canciones[j].nombre}</h2>
+	  //             <p>${canciones[j].ruta}</p>
+	  //             <p>Reproducciones: ${canciones[j].reproducciones}</p
+	  //             <p>Icono: ${canciones[j].icono}</p>
+
+
+	  //   }
+
+		//Modifica el DOM agregando el html generado
+		//document.getElementById("canciones").innerHTML = html
+
+	  })
 };
 
 
+$(document).ready(function () {
+	traerDatos()
+});
+
+console.log('correcto');
 
 
 
